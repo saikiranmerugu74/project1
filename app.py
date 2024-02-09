ab9064ac9e9ea3ce14f30b0e76fa5d62c782a961
saikiran <saikiran.merugu74@gmail.com>
2024-02-09 14:44:36 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+# app.py
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+=======
+# app.py
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+>>>>>>> 5399c9df8a58251b2ff5a0c4350e4a928d2c0f27
