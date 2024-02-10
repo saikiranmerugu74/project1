5fd2c439866b58ca00c3dd4d1f90b08f86cc0eda
saikiranmerugu74 <147452666+saikiranmerugu74@users.noreply.github.com>
2024-02-09 19:01:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,13 @@
-<<<<<<< HEAD
 # app.py
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return 'Hello, World! This is my first Flask web application.'
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-# app.py
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> 5399c9df8a58251b2ff5a0c4350e4a928d2c0f27
