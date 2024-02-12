893374523044261d596e967a56f7a6d71b2e2ff3
saikiran <saikiran.merugu74@gmail.com>
2024-02-12 16:26:24 -0500

    pushing latest files

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # app.py
 
 from flask import Flask, render_template
@@ -11,17 +10,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-# app.py
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> 5399c9df8a58251b2ff5a0c4350e4a928d2c0f27
