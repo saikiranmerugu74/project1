d3bb02901840e77d912c7c01573afa56783e8c11
saikiran <saikiran.merugu74@gmail.com>
2024-02-13 15:27:04 -0500

    adding jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,44 @@
+pipeline {
+    environment {
+        registry = "saikiranmerugu74/sai1" //To push an image to Docker Hub, you must first name your local image using your Docker Hub username and the repository name that you created through Docker Hub on the web.
+        registryCredential = 'DOCKERHUB'
+        //githubCredential = 'GITHUB'
+        dockerImage = ''
+    }
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scmGit(branches: [[name: '*/main']],
+                extensions: [],
+                userRemoteConfigs: [[url: 'https://github.com/saikiranmerugu74/project1.git']]) 
+                
+            }
+        }
+        stage('Build Image') {
+            steps {
+                script {
+                    img = registry + ":${env.BUILD_ID}"
+                    println ("${img}")
+                    dockerImage = docker.build("${img}")
+                }
+            }
+        }
+
+        stage('Push To DockerHub') {
+            steps {
+                script {
+                    docker.withRegistry( 'https://registry.hub.docker.com ', registryCredential ) {
+                    dockerImage.push()
+                    }
+                }
+            }
+        }
+
+        stage('Deploy') {
+           steps {
+                sh label: '', script: "docker run -d --name ${JOB_NAME} -p 5000:5000 ${img}"
+          }
+        }
+    }
+}
