ca02393963bcc539e6cdefd4c4382750103d54b0
saikiran <saikiran.merugu74@gmail.com>
2024-02-14 16:55:10 -0500

    updating test case

--- pytesttestcase.py
+++ pytesttestcase.py
@@ -1,7 +1,8 @@
 import pytest
-from selenium import webdriver
+'''
+#from selenium import webdriver
 
-@pytest.fixture
+#@pytest.fixture
 def browser():
     # This fixture sets up and tears down the Selenium WebDriver
     driver = webdriver.Chrome()  # You can choose a different driver (Firefox, etc.) based on your preference
@@ -23,3 +24,13 @@ def test_validate_text_on_web_page(browser):
 
     # Validate that the actual text matches the expected text
     assert actual_text == expected_text, f"Expected text: {expected_text}, Actual text: {actual_text}"
+
+
+    # test_app.py'''
+
+def test_flask_app_text():
+    expected_text = "Hello, World!\nThis is my Flask web application."
+    actual_text = home()
+
+    assert actual_text == expected_text, f"Expected: {expected_text}\nActual: {actual_text}"
+
