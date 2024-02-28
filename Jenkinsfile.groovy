cb8cbf8258ff50aedc6d83f93d93bf802917f4d5
saikiran <saikiran.merugu74@gmail.com>
2024-02-28 12:53:21 -0500

    added git checkout part on ec2

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -59,7 +59,7 @@ pipeline {
                 script {
                     sshagent(['deployserver']) {
                         //sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'docker run -d -p 8000:8000 --name newpythonwebapp ${img}'"
-                        sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'checkout scmGit(branches: [[name: '*/main']],]"
+                        sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'checkout scmGit(branches: [[name: '*/main']],]'"
                         sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'extensions: [],'"
                         sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'userRemoteConfigs: [[url: 'https://github.com/saikiranmerugu74/project1.git']]) '"
                         sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'ls'"
