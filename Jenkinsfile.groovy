3adea693515fbc82c07f6bbc48d3b45cedf24e9f
saikiran <saikiran.merugu74@gmail.com>
2024-03-06 10:53:32 -0500

    updated deploy server settings

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -58,7 +58,8 @@ pipeline {
                     sshagent(['deployserver']) {
                         sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'git clone https://github.com/saikiranmerugu74/project1.git -b main'"
                         sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'cp -r project1/* /home/ubuntu'"
-                        sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'ls'"
+                        //sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'ls'"
+                        sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'pwd'"
                         sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_HOST} 'docker-compose up -d --build'"
                         }
 
